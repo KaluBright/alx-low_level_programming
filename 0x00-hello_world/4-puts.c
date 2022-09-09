1e48044a9b062ea7939b6a43f24b91003d82188f
KaluBright <feelbok@gmail.com>
2022-09-08 18:30:40 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- Program entry point
+ *
+ * Return: if no errors, return zero otherwise return non-zero value
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	*Return(0);
+}
