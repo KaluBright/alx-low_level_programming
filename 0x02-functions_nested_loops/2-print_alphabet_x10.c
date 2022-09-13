cd911c87f280c540dd546a1e292758c022bab0e7
KaluBright <feelbok@gmail.com>
2022-09-13 15:52:13 -0700

    10xalphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,13 @@
 
 void print_alphabet_x10(void)
 {
-	int alpha;
-	int alpha2;
+	int count = 0;
+	char letter;
 
-	for (alpha = 0; alpha <= 9; alpha++)
+	while (count++ <= 9)
 	{
-		for (alpha2 = 0; alpha <= 9; alpha2++)
-		{
-			_putchar(alpha2);
-		}
+		for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 		_putchar('\n');
 	}
 }
